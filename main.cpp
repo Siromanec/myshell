0e973038fc86c3665bbcc5ae74515f16ddeb9d7c
Siromanec <serhii.ivanov@ucu.edu.ua>
2023-10-16 23:38:19 +0300

    fixed wildcards

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <iostream>
+#include <memory>
 #include "options_parser.h"
 #include "MyShell.hpp"
 #include "ScriptRunner.hpp"
@@ -11,15 +12,14 @@ int main(int argc, char *argv[]) {
   std::unique_ptr<AbstractRunner> runner;
   if (argc == 1) {
     auto myshell = new MyShell();
-    auto prompt_putter = std::unique_ptr<PromptPutter>(new PromptPutter());
+    auto prompt_putter = std::make_unique<PromptPutter>();
     myshell->setPromptPutter(std::move(prompt_putter));
     runner = std::unique_ptr<AbstractRunner>(myshell);
   }
   else{
 //TODO check if the argv[1] has extension ".msh" (defined in AbstractRunner) and pass the right arguments
-    command_line_options_t command_line_options{argc, argv};
-    std::cout << "A flag value: " << command_line_options.get_A_flag() << std::endl;
-    runner = std::make_unique<ScriptRunner>(argc, argv);
+
+    runner = std::make_unique<ScriptRunner>(argc-1, argv+1);
   }
   runner->run();
   return 0;
--- myshell/AbstractRunner.cpp
+++ myshell/AbstractRunner.cpp
@@ -10,146 +10,127 @@
 #include "ScriptCommand.hpp"
 
 void AbstractRunner::run() {
-    bool flag = true;
-    while (flag) {
-        auto command = getNextCommand(flag);
-        if (command == nullptr) { //TODO check if unique ptr can be nullptr
-            return;
-        }
-        command->execute();
+  bool flag = true;
+  while (flag) {
+    auto command = getNextCommand(flag);
+    if (command == nullptr) { //TODO check if unique ptr can be nullptr
+      return;
     }
+    command->execute();
+  }
 }
 
 std::unique_ptr<Command> AbstractRunner::getNextCommand(bool &flag) {
-    auto commandArgv = readNext(flag);
+  auto commandArgv = readNext(flag);
 
 #ifdef DEBUG
-    std::cout<<"argv: ";
-    for (const auto& s: commandArgv){
-      std::cout<< s<<'\t';
-    }
-    std::cout<< std::endl;
+  std::cout<<"argv: ";
+  for (const auto& s: commandArgv){
+    std::cout<< s<<'\t';
+  }
+  std::cout<< std::endl;
 #endif
 
-    switch (getCommandType(commandArgv)) {
-        case INTERNAL:
-            return std::make_unique<InternalCommand>(std::move(commandArgv));
-        case EXTERNAL:
-            return std::make_unique<ExternalCommand>(std::move(commandArgv));
-        case SCRIPT:
-            return std::make_unique<ScriptCommand>(std::move(commandArgv));
-        default:
-            std::cerr << "*** unknown command type" << std::endl;
-            throw std::exception();
-    }
+  switch (getCommandType(commandArgv)) {
+    case INTERNAL:
+      return std::make_unique<InternalCommand>(std::move(commandArgv));
+    case EXTERNAL:
+      return std::make_unique<ExternalCommand>(std::move(commandArgv));
+    case SCRIPT:
+      return std::make_unique<ScriptCommand>(std::move(commandArgv));
+    default:
+      std::cerr << "*** unknown command type" << std::endl;
+      throw std::exception();
+  }
 }
 
 const std::vector<std::string> AbstractRunner::internal_commands{
-        "merrno",
-        "mpwd",
-        "mcd",
-        "mexit",
-        "mecho",
-        "mexport",
-        ".",
+    "merrno",
+    "mpwd",
+    "mcd",
+    "mexit",
+    "mecho",
+    "mexport",
+    ".",
 };
 
 const std::string AbstractRunner::name{"myshell"};
 const std::string AbstractRunner::extension{".msh"};
 
 std::regex wildcardToRegex(const std::string &wildcard, bool caseSensitive) {
-    // Note It is possible to automate checking if filesystem is case sensitive or not (e.g. by performing a test first time this function is ran)
-    std::string regexString{wildcard};
-    // Escape all regex special chars:
-    regexString = std::regex_replace(regexString, std::regex("\\\\"), "\\\\");
-    regexString = std::regex_replace(regexString, std::regex("\\^"), "\\^");
-    regexString = std::regex_replace(regexString, std::regex("\\."), "\\.");
-    regexString = std::regex_replace(regexString, std::regex("\\$"), "\\$");
-    regexString = std::regex_replace(regexString, std::regex("\\|"), "\\|");
-    regexString = std::regex_replace(regexString, std::regex("\\("), "\\(");
-    regexString = std::regex_replace(regexString, std::regex("\\)"), "\\)");
-    regexString = std::regex_replace(regexString, std::regex("\\{"), "\\{");
-    regexString = std::regex_replace(regexString, std::regex("\\{"), "\\}");
-    regexString = std::regex_replace(regexString, std::regex("\\["), "\\[");
-    regexString = std::regex_replace(regexString, std::regex("\\]"), "\\]");
-    regexString = std::regex_replace(regexString, std::regex("\\+"), "\\+");
-    regexString = std::regex_replace(regexString, std::regex("\\/"), "\\/");
-    // Convert wildcard specific chars '*?' to their regex equivalents:
-    regexString = std::regex_replace(regexString, std::regex("\\?"), ".");
-    regexString = std::regex_replace(regexString, std::regex("\\*"), ".*");
+  // Note It is possible to automate checking if filesystem is case sensitive or not (e.g. by performing a test first time this function is ran)
+  std::string regexString{wildcard};
+  // Escape all regex special chars:
+  regexString = std::regex_replace(regexString, std::regex("\\\\"), "\\\\");
+  regexString = std::regex_replace(regexString, std::regex("\\^"), "\\^");
+  regexString = std::regex_replace(regexString, std::regex("\\."), "\\.");
+  regexString = std::regex_replace(regexString, std::regex("\\$"), "\\$");
+  regexString = std::regex_replace(regexString, std::regex("\\|"), "\\|");
+  regexString = std::regex_replace(regexString, std::regex("\\("), "\\(");
+  regexString = std::regex_replace(regexString, std::regex("\\)"), "\\)");
+  regexString = std::regex_replace(regexString, std::regex("\\{"), "\\{");
+  regexString = std::regex_replace(regexString, std::regex("\\{"), "\\}");
+  regexString = std::regex_replace(regexString, std::regex("\\["), "\\[");
+  regexString = std::regex_replace(regexString, std::regex("\\]"), "\\]");
+  regexString = std::regex_replace(regexString, std::regex("\\+"), "\\+");
+  regexString = std::regex_replace(regexString, std::regex("\\/"), "\\/");
+  // Convert wildcard specific chars '*?' to their regex equivalents:
+  regexString = std::regex_replace(regexString, std::regex("\\?"), ".");
+  regexString = std::regex_replace(regexString, std::regex("\\*"), ".*");
 
-    return std::regex(regexString, caseSensitive ? std::regex_constants::ECMAScript : std::regex_constants::icase);
+#ifdef DEBUG
+  std::cerr << regexString << std::endl;
+#endif
+  return std::regex(regexString, caseSensitive ? std::regex_constants::ECMAScript : std::regex_constants::icase);
 }
 
 bool wildmatch(const std::string &input, const std::string &wildcard) {
-    auto rgx = wildcardToRegex(wildcard);
-    return std::regex_match(input, rgx);
+  auto rgx = wildcardToRegex(wildcard);
+  return std::regex_match(input, rgx);
 }
 
 static bool has_wildcards(const bfs::wpath &path) {
-    //TODO підстановка вайлдкарлів
-//  return wildmatch();
 
-    for (size_t i = 0; i < path.string().size(); ++i) {
-        switch (path.string().at(i)) {
-            case '*':
-                return true;
-            case '?':
-                return true;
-            case '[':
-                return true;
-        }
-
+  for (char i: path.string()) {
+    switch (i) {
+      case '*':
+        return true;
+      case '?':
+        return true;
+      case '[': //TODO posible bug here
+        return true;
     }
-    return false;
+  }
+
+
+  return false;
 }
 
-/**
- * modifies s!
- * @param s
- * @return
- */
-
-
 std::vector<std::string> unfold_string(char *s) {
-    //TODO
-    //TODO підстановка вайлдкарлів
 
-    // не всі стрінги це шлях
-//  bfs::path(s);
-// the wildcard substitution must occur before or after canonization of path?
-// does nothing if no such file or directory returns the string
-// if file ->wildcard sub
-//iterate over path directories
-// if any contain wildcard pattern -> seek and substitute
 #ifdef DEBUG
-    std::cout << s << std::endl;
+  std::cout << s << std::endl;
 #endif
 
+  if (has_wildcards(s)) {
+    std::vector<std::string> matching_files;
 
-        const bfs::wpath path = s;
-        const bfs::wpath &parent_path = path.parent_path();
-        bool has_wildcard = has_wildcards(path.filename());
+    const bfs::wpath path{s};
+    auto canonicalPath = boost::filesystem::canonical(path.parent_path());
+    const auto wildcard = path.filename().string(); //using reference gives unexpected output (it changes with each call to filename().string())
 
-        boost::filesystem::wpath canonicalPath;
+    for (const auto& x: bfs::directory_iterator(canonicalPath)) {
+      if (wildmatch(x.path().filename().string(), wildcard))
+        matching_files.push_back(x.path().filename().string());
+    }
 
-        if (has_wildcard) {
+    if(!matching_files.empty())
+      return matching_files;
+  }
 
-            std::vector<std::string> matching_files;
-            canonicalPath = boost::filesystem::canonical(parent_path);
-            const auto &wildcard = path.filename().string();
-
-            for (auto &&x: bfs::directory_iterator(canonicalPath)) {
-                if (wildmatch(x.path().filename().string(), wildcard))
-                    matching_files.push_back(x.path().string());
-            }
-            return matching_files;
-
-        }
-
-    /*catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }*/
-    return {s};
+  /*catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+  }*/
+  return {s};
 
 }
