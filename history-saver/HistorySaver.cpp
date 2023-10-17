1e094e36cb4d9d21f92f4e58309ba7aafd58b7bb
Siromanec <serhii.ivanov@ucu.edu.ua>
2023-10-17 15:19:47 +0300

    added mycat

--- history-saver/HistorySaver.cpp
+++ history-saver/HistorySaver.cpp
@@ -1,23 +1,23 @@
+////
+//// Created by ADMIN on 17-Oct-23.
+////
+//#include <string>
+//#include <boost/filesystem/path.hpp>
+//#include <readline/history.h>
+//#include <iostream>
+//#include <fstream>
 //
-// Created by ADMIN on 17-Oct-23.
+//#include "HistorySaver.hpp"
 //
-#include <string>
-#include <boost/filesystem/path.hpp>
-#include <readline/history.h>
-#include <iostream>
-#include <fstream>
-
-#include "HistorySaver.hpp"
-
-HistorySaver::HistorySaver() {
-  if(history_truncate_file(path.c_str(), MAXLINES) || read_history(path.c_str())){
-    std::cerr << "*** could not truncate or load history ***" << std::endl;
-  }
-}
-
-HistorySaver::~HistorySaver() {
-  if(write_history(path.c_str())){
-    std::cerr << "*** could not save history ***" << std::endl;
-  };
-}
-const std::string HistorySaver::path{"~/.msh_history"};
+//HistorySaver::HistorySaver() {
+//  if(history_truncate_file(path.c_str(), MAXLINES) || read_history(path.c_str())){
+//    std::cerr << "*** could not truncate or load history ***" << std::endl;
+//  }
+//}
+//
+//HistorySaver::~HistorySaver() {
+//  if(write_history(path.c_str())){
+//    std::cerr << "*** could not save history ***" << std::endl;
+//  };
+//}
+//const std::string HistorySaver::path{"~/.msh_history"};
